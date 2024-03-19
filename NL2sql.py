1b1981944f99e479f6a9583d2c98866567cc8224
Ravindra Singh Kushwaha <94731464+ravindrakush11@users.noreply.github.com>
2024-03-19 17:30:28 +0530

    Add files via upload

--- NL2sql.py
+++ NL2sql.py
@@ -1,32 +1,30 @@
 import os
-from sqlalchemy.dialects.postgresql import JSON
-
+import openai
 from langchain.chains import create_sql_query_chain
 from langchain_openai import ChatOpenAI
-
 from langchain_community.utilities.sql_database import SQLDatabase
 from langchain_community.tools.sql_database.tool import QuerySQLDataBaseTool
-
 from operator import itemgetter
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import PromptTemplate
 from langchain_core.runnables import RunnablePassthrough
-
+from flask_cors import CORS
 from flask import Flask, request, jsonify
-import psycopg2 
+
 
 os.environ["OPENAI_API_KEY"] = "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"
 os.environ["LANGCHAIN_TRACING_V2"] = "true"
 os.environ["LANGCHAIN_API_KEY"] = "ls__7a2e134f3afc4daeaed6fcbe2fb11fd2"
 
+db = SQLDatabase.from_uri("postgresql://postgres:123@localhost:5432/flask_database")
+# "postgresql://postgres:redhat@206.189.136.112:5432/Sample_Data_Text2SQL"
 
-
-db = SQLDatabase.from_uri("postgresql://postgres:123@localhost/flask_database")
 llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
 generate_query = create_sql_query_chain(llm, db)  
 execute_query = QuerySQLDataBaseTool(db=db)
 
 app = Flask(__name__)
+CORS(app)
     
 def execute_question(question):
     try:
@@ -58,14 +56,15 @@ def answer_sql():
         data = request.json
         user_question = data.get('question')
     
-        if not user_question:
-                return jsonify({"error": "Invalid input: Question is missing"}), 400
+        if not data or not user_question:
+            return jsonify({"status":400, "error": "Invalid input: Question is missing"}), 400
             
         output = execute_question(user_question)
+        output["status"]=200
         return jsonify(output)
     
     except Exception as e:
-        return jsonify({"error": str(e)}), 500  
+        return jsonify({"status":200,"error": str(e)}), 500  
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
